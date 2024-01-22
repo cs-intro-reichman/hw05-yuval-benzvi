5a78f59cc0b93fbef55535ddaba892aa5d1c36ff
Yuval Ben-Zvi <yuval.benzvi@post.runi.ac.il>
2024-01-22 09:38:02 +0200

    fix

--- GameOfLife.java
+++ GameOfLife.java
@@ -137,8 +137,8 @@ public class GameOfLife {
 	
 	// Prints the board. Alive and dead cells are printed as 1 and 0, respectively.
     public static void print(int[][] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[i].length; j++) {
+		for (int i = 1; i < arr.length-1; i++) {
+			for (int j = 1; j < arr[i].length-1; j++) {
 				System.out.printf("%3s", arr[i][j]);
 			}
 			System.out.println();
